6b144d767e495ce2d3a435d787a012ef35413e67
anonymous-researcher28 <anonymous.reasearcher1@gmail.com>
2026-04-18 00:35:19 -0400

    Create custom_sampler.py

---
+++ custom_sampler.py
@@ -0,0 +1,34 @@
+import torch
+from torch import Tensor
+from torch.utils.data import Sampler
+from typing import Sequence, Iterator
+
+class SubsetWeightedRandomSampler(Sampler[int]):
+    weights: Tensor
+    num_samples: int
+    replacement: bool
+
+    def __init__(self, weights: Sequence[float],
+                 indices: Sequence[int],
+                 replacement: bool = True, generator=None) -> None:
+        if not isinstance(replacement, bool):
+            raise ValueError("replacement should be a boolean value, but got "
+                             "replacement={}".format(replacement))
+
+        weights_tensor = torch.as_tensor(weights, dtype=torch.double)
+        if len(weights_tensor.shape) != 1:
+            raise ValueError("weights should be a 1d sequence but given "
+                             "weights have shape {}".format(tuple(weights_tensor.shape)))
+
+        self.weights = weights_tensor[indices]
+        self.num_samples = len(indices)
+        self.indices = indices
+        self.replacement = replacement
+        self.generator = generator
+
+    def __iter__(self) -> Iterator[int]:
+        rand_tensor = torch.multinomial(self.weights, self.num_samples, self.replacement, generator=self.generator)
+        yield from iter(self.indices[rand_tensor].tolist())
+
+    def __len__(self) -> int:
+        return self.num_samples
